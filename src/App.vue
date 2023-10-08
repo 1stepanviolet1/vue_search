4d4f392d5a51eb1c35963b0cecca313280f702dd
1stepanviolet1 <stepanviolet@gmail.com>
2023-10-08 20:04:40 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <button 
+      type="btn" 
+      class="add-word-btn btn btn-success"
+      @click="add_new_word()"
+    >
+      Add word
+    </button>
+    <div class="text-center">
+      <SearchWords :words="words"/>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import SearchWords from './components/SearchWords.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    SearchWords
+  },
+
+  data() {
+    return {
+      words: ['Макс', 'Даниил', 'Данила', 'Егор', 'Мариша']
+    }
+  },
+
+  methods: {
+    add_new_word() {
+      const new_word = prompt("Введите новое слово: ");
+      
+      if (!new_word) return;
+
+      this.words.push(new_word);
+    
+    }
+  }
+}
+
+</script>
+
+
+<style>
+
+.add-word-btn {
+  top: 0.3em;
+  right: 0.3em;
+  position: absolute;
+}
+
+</style>
---
+++ src/components/SearchWords.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="search">
+
+    <div class="header">
+      <h1>Поиск слов</h1>
+    </div>
+
+    <input 
+      type="text"
+      @input="search_word = $event.target.value"
+      placeholder="Word"
+    />
+
+    <div class="words-info">
+      <p>
+        Всего слов: {{ words.length }}
+        <label class="divider">|</label> 
+        Совпадений: {{ searching_words.length }}
+      </p>
+    </div>
+
+    <hr>
+
+    <div class="word-list">
+      <p
+        v-for="(word, i) in searching_words"
+        :key="i"
+      >
+        {{ word }} <br>
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchWords',
+
+  props: {
+    words: Array
+  },
+
+  data() {
+    return {
+      search_word: ''
+    }
+  },
+
+  computed: {
+    searching_words() {
+      return this.words.filter(
+        word => word.toLowerCase().includes(this.search_word.toLowerCase())
+      );
+    },
+
+    name_matches() {
+      return this.search_word == '' 
+                    ? 0
+                    : this.searching_words.length;
+    }
+
+  }
+  
+}
+
+</script>
+
+<style>
+
+.search {
+  margin-top: 5em;
+}
+
+
+.header {
+  margin-bottom: 2em;
+}
+
+.words-info {
+  margin-top: 0.2em;
+  margin-bottom: 2em;
+  margin-left: 0.7em;
+}
+
+.divider {
+  margin-left: 1.5em;
+  margin-right: 1.5em;
+}
+
+.word-list {
+  margin-top: 2em;
+}
+
+</style>
